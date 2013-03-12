c3ab0642b721af6842026b20c6d4379cb1a9427c
Andrew M <me@r3nya.ru>
2013-03-13 00:11:39 +0400

    Updated code :repeat:

--- game.rb
+++ game.rb
@@ -1,32 +1,20 @@
 require 'sinatra'
 
 before do
+	headers "Content-Type" => "text/html; charset=utf-8"
 	@defeat = { rock: :scissors, paper: :rock, scissors: :paper }
 	@throws = @defeat.keys
 end
 
 get '/' do
-	erb :index
+	@title = "Rock-paper-scissors!"
+	erb :links
 end
 
 get '/throw/:type' do
-	player_throw = params[:type].to_sym
-
-	unless @throws.inlude?(player_throw)
+	@player_throw = params[:type].to_sym
+	unless @throws.include?(@player_throw)
 		halt 403, "You must throw one of the followinf: #{@throws}"
 	end
-
-	computer_throw = @throws.sample
-
-	if player_throw == computer_throw
-		"You tied with the computer. Try again!"
-	elsif computer_throw == @defeat[player_throw]
-		"Nicely done! #{player_throw} beats #{computer_throw}!"
-	else
-		"Ouch! #{computer_throw} beats #{player_throw} ... Better luck next time!"
-	end
-end
-
-not_found do
-	halt 404, "Whoops! Error 404"
+	erb :result
 end
